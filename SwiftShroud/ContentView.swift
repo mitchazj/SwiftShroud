7e83ba8d38782425acdda46c5b95382eefc5f24d
Mitchell Johnson <mjohnson@canva.com>
2023-02-06 21:32:10 +1000

    Initial Commit

---
+++ SwiftShroud/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftShroud
+//
+//  Created by Mitchell Johnson on 6/2/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftShroud/SwiftShroudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftShroudApp.swift
+//  SwiftShroud
+//
+//  Created by Mitchell Johnson on 6/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftShroudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
